d98a78aed4b1c287fd15df9587bd36e454406d06
qa-vustr-2-nlyf@example.com <qa-vustr-2-nlyf@example.com>
2023-02-01 15:14:05 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15093.stoplight-dev.com/api/v1/projects/cHJqOjY0NA/spectral.js?branch_slug=master&token=6ae6e743-64ed-4ea2-8ad5-3a8a45a731a3";
+export default { extends: ruleset };
